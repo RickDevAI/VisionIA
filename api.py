a8ba7f28c32f003cc9af66319a20ed1889d2fdfc
RickDevAI <ricardorabelo10.rr@gmail.com>
2026-05-29 19:16:39 -0400

    Update api.py

--- api.py
+++ api.py
@@ -40,6 +40,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://vision-ia-flame.vercel.app",
+        "https://*.vercel.app",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://localhost:5500",
