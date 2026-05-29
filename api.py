07add853882fb59435c3c0298829917ffac6eb89
RickDevAI <ricardorabelo10.rr@gmail.com>
2026-05-29 00:46:54 -0400

    Update api.py

--- api.py
+++ api.py
@@ -38,7 +38,13 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://vision-ia-flame.vercel.app",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+    ],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
